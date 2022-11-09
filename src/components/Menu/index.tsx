0e84a2abec032d25d8127868952a9385f1950277
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-09 06:56:19 -0300

    Cria componente de Menu e coloca a Logo

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "../../assets/Logo";
+
+import { StyledMenu } from './styles'
+
+export function Menu() {
+  return (
+    <StyledMenu>
+      <div>
+        <Logo />
+      </div>
+    </StyledMenu>
+  )
+}
