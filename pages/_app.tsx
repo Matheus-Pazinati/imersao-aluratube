56f62d984a0fd60bc852057dba98fae3ebde2cbb
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-12 05:18:51 -0300

    Cria um contexto para compartilhar as informacoes do tema da aplicacao

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
 import type { AppProps } from 'next/app'
+import { useContext } from 'react'
 
 import { ThemeProvider } from 'styled-components'
+
+import { ColorModeProvider, ColorModeContext, ColorModeProviderProps } from '../src/contexts/ColorModeContext'
+
 import { CSSReset } from '../src/styles/global'
 import { defaultTheme } from '../src/styles/themes/default'
 
-export default function App({ Component, pageProps }: AppProps) {
+function ProviderWrapper({ children }: ColorModeProviderProps) {
   return (
-    <ThemeProvider theme={defaultTheme.light}>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ColorModeProvider>
+      {children}
+    </ColorModeProvider>
   )
+}
 
+function MyApp({ Component, pageProps }: AppProps) {
+  const { colorMode } = useContext(ColorModeContext)
+  console.log(colorMode)
+  return (
+      <ThemeProvider theme={defaultTheme[colorMode]}>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ThemeProvider>
+  )
+}
+
+export default function _App(props: AppProps) {
+  return (
+    <ProviderWrapper>
+      <MyApp {...props} />
+    </ProviderWrapper>
+  )
 }
---
+++ src/contexts/ColorModeContext.tsx
@@ -0,0 +1,31 @@
+import { useState, createContext, ReactNode } from 'react'
+import { defaultTheme } from '../styles/themes/default'
+
+interface ColorContextProps {
+  colorMode: keyof typeof defaultTheme
+  changeColorMode: () => void
+}
+
+export interface ColorModeProviderProps {
+  children: ReactNode
+}
+
+export const ColorModeContext = createContext({} as ColorContextProps)
+
+export function ColorModeProvider({ children }: ColorModeProviderProps) {
+  const [colorMode, setColorMode] = useState('dark' as keyof typeof defaultTheme)
+
+  function changeColorMode() {
+    if (colorMode === 'light') {
+      setColorMode('dark')
+    } else {
+      setColorMode('light')
+    }
+  }
+
+  return (
+    <ColorModeContext.Provider  value={{ colorMode, changeColorMode}}>
+      {children}
+    </ColorModeContext.Provider>
+  )
+}
