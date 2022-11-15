cf9255d5f0f94135850c9cdc31254da7831203f0
Matheus Pazinati <matheuspazinati@gmail.com>
2022-11-15 06:42:27 -0300

    Cria um servico para separar o que diz respeito as operacoes no banco

--- src/components/RegisterVideo/index.tsx
+++ src/components/RegisterVideo/index.tsx
@@ -1,14 +1,9 @@
 import { FormEvent, useState } from "react";
 
-import { createClient } from '@supabase/supabase-js'
-
 import { StyledRegisterVideo } from "./styles";
 
 import { useForm } from '../../hooks/useForm'
-
-const supabaseUrl = "https://gooubqmssqikddgevorg.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdvb3VicW1zc3Fpa2RkZ2V2b3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg0MTUzMDMsImV4cCI6MTk4Mzk5MTMwM30.S-mW3D2wCmqbRh0EzIVvHD45ak5L8fuQQf3ghUTebcs"
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { VideoProps, videoService } from "../../services/videoService";
 
 function getThumbFromVideo(url: string) {
   return `https://img.youtube.com/vi/${url.split("v=")[1]}/hqdefault.jpg`
@@ -17,6 +12,7 @@ function getThumbFromVideo(url: string) {
 export function RegisterVideo() {
   const [modalVisible, setModalVisible] = useState(false)
   const formRegister = useForm()
+  const service = videoService()
 
   function closeModal() {
     setModalVisible(false)
@@ -36,17 +32,18 @@ export function RegisterVideo() {
           <form
             onSubmit={(e: FormEvent) => {
               e.preventDefault()
-              console.log(formRegister.formData.title, formRegister.formData.url, formRegister.formData.type)
-              supabase.from("video").insert({
+              const formValues: VideoProps = {
                 title: formRegister.formData.title,
                 url: formRegister.formData.url,
                 thumb: getThumbFromVideo(formRegister.formData.url),
                 type: formRegister.formData.type
-              })
+              }
+              service.createVideo(formValues)
               .then((data) => {
                 //Criar um alerta
                 console.log(data)
               })
+
               formRegister.clearForm()
               closeModal()
             }}
---
+++ src/services/videoService.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = "https://gooubqmssqikddgevorg.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdvb3VicW1zc3Fpa2RkZ2V2b3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg0MTUzMDMsImV4cCI6MTk4Mzk5MTMwM30.S-mW3D2wCmqbRh0EzIVvHD45ak5L8fuQQf3ghUTebcs"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export interface VideoProps {
+  title: string,
+  url: string,
+  thumb: string,
+  type: string
+}
+
+export function videoService() {
+  return {
+    createVideo(data: VideoProps) {
+      return supabase.from("video").insert(data)
+    }
+  }
+}
